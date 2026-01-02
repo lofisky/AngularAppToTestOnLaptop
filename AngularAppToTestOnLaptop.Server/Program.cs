2613225d931620901cdfba0693e242868aa9ed8c
Ezel Inan <inanezel@Outlook.com>
2026-01-02 23:21:32 +0000

    Connected to postgresql db, ensured that the connection string is ignored by git for password security, replaced weatherforecast controller with a db testing controller to ensure successful db access

--- AngularAppToTestOnLaptop.Server/Program.cs
+++ AngularAppToTestOnLaptop.Server/Program.cs
@@ -1,4 +1,8 @@
+using AngularAppToTestOnLaptop.Server.Database;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.secret.json", optional: true, reloadOnChange: true);
 
 // Add services to the container.
 
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<databaseAccess>();
+
 
 var app = builder.Build();
 
