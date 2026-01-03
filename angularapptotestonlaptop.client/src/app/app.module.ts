c0b60699be706a08b45ed667e6ae416ec90e4daf
Ezel Inan <inanezel@Outlook.com>
2026-01-03 22:35:46 +0000

    Created a register component, utilising previous log in page html and css and adjusting to fit styling consistency and wireframe detailing. Hid the links on the navbar like log in page but for this profile creation page. Added register page to routing and ensured that the sign up button on log in page directs to the register page.

--- angularapptotestonlaptop.client/src/app/app.module.ts
+++ angularapptotestonlaptop.client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './presentation/navbar/navbar.component';
 import { LoginComponent } from './presentation/login-register/login/login.component';
+import { RegisterComponent } from './presentation/login-register/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
