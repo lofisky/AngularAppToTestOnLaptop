ec9dd161b075aa39c4f35efdb90e2468d6afcf02
Ezel Inan <inanezel@Outlook.com>
2026-01-04 19:10:27 +0000

    Populated user/admin repository

--- AngularAppToTestOnLaptop.Server/Persistence/UserAndAdminRepository.cs
+++ AngularAppToTestOnLaptop.Server/Persistence/UserAndAdminRepository.cs
@@ -2,5 +2,10 @@
 {
     public class UserAndAdminRepository
     {
+        public int userId { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
     }
 }
