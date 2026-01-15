49e1ebaf07e235eda3b6c349d2f64b7719276b76
Ezel Inan <inanezel@Outlook.com>
2026-01-15 13:25:56 +0000

    Made basic frontend for register following login format, basic skeleton

--- angularapptotestonlaptop.client/src/app/presentation/login-register/register/register.component.ts
+++ angularapptotestonlaptop.client/src/app/presentation/login-register/register/register.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  username: string = '';
+  userEmail: string = '';
+  userPassword: string = '';
+  registerMessage: string = '';
 
+  constructor(private router: Router, private http: HttpClient) { }
+
+  createProfileRedirect() {
+    this.http.post<boolean>('https://localhost:7109/api/auth/register', {
+      username: this.username,
+      userEmail: this.userEmail,
+      userPassword: this.userPassword
+    }).subscribe(success => {
+      if (success) {
+        this.registerMessage = `Profile Created! Welcome to DSArena, ${this.userEmail}`;
+        console.log("register success!Q!");
+      }
+      else {
+        alert('Cannot create profile. Select unique credentials');
+      }
+    }, error => {
+      this.registerMessage = 'errorrrr';
+      console.error(error);
+    });
+  }
 }
