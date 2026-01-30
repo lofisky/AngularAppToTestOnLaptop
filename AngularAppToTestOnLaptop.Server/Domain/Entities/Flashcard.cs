f97282c60e7140f84bdb03ca3fd072ec4eb3a467
Ezel Inan <inanezel@Outlook.com>
2026-01-30 13:58:29 +0000

    Added frontend for flashcard sets and made minor backend fixes.

---
+++ AngularAppToTestOnLaptop.Server/Domain/Entities/Flashcard.cs
@@ -0,0 +1,10 @@
+﻿namespace AngularAppToTestOnLaptop.Server.Domain.Entities
+{
+    public class Flashcard
+    {
+        public int FlashcardId { get; set; }
+        public string FrontText { get; set; }
+        public string BackText { get; set; }
+        public int FlashcardSetId { get; set; }
+    }
+}
--- angularapptotestonlaptop.client/src/app/presentation/educational-tools/flashcards/flashcards.component.ts
+++ angularapptotestonlaptop.client/src/app/presentation/educational-tools/flashcards/flashcards.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-flashcards',
   templateUrl: './flashcards.component.html',
   styleUrls: ['./flashcards.component.css']
 })
-export class FlashcardsComponent {
+export class FlashcardsComponent implements OnInit {
+  flashcardSets: any[] = [];
+  selectedTopic: string = 'Two pointers'; //default topic
+  flashcardMessage: string = '';
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchFlashcards(this.selectedTopic);
+  }
+
+  onTopicChange(event: any): void {
+    this.selectedTopic = event.target.value;
+    this.fetchFlashcards(this.selectedTopic);
+  }
+
+  fetchFlashcards(topic: string): void {
+    this.flashcardMessage = '';
+
+    this.http.get<any[]>(`https://localhost:7109/api/flashcards/getFlashcardSets?topic=${topic}`)
+      .subscribe(flashcards => {
+        if (flashcards && flashcards.length > 0) {
+          this.flashcardSets = flashcards;
+        }
+        else {
+          this.flashcardSets = [];
+          this.flashcardMessage = 'No flashcard available for selected topic';
+        }
+      }, error => {
+        this.flashcardMessage = 'Error fetching flashcards :(';
+        console.error('Error fetching flashcards: ', error);
+      })
+  }
 }
